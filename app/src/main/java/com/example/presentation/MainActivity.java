21a33b741cbbc1f1c9da6ee67fd2d9e26a83df59
D.J. Saderholm <catsdgs@disr.it>
2021-12-16 12:23:10 -0700

    sfdsf

---
+++ app/src/main/java/com/example/presentation/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.presentation;
+
+import android.content.Context;
+import android.hardware.display.DisplayManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Display;
+
+public class MainActivity extends AppCompatActivity {
+    private DisplayManager displayManager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initVideo();
+    }
+    private void initVideo() {
+        if (displayManager == null) {
+            displayManager = (DisplayManager) getSystemService(Context.DISPLAY_SERVICE);
+            Display[] displays = displayManager.getDisplays();
+            if (displays.length > 1) {
+                ScreenPresentation screenPresentation = new ScreenPresentation(MainActivity.this, displays[1]);
+                screenPresentation.show();
+            }
+        }
+
+    }
+
+
+}
---
+++ app/src/main/java/com/example/presentation/ScreenPresentation.java
@@ -0,0 +1,66 @@
+package com.example.presentation;
+
+import android.annotation.SuppressLint;
+import android.app.Presentation;
+import android.content.Context;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Display;
+import android.view.KeyEvent;
+import android.webkit.CookieManager;
+import android.webkit.CookieSyncManager;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class ScreenPresentation extends Presentation {
+    public ScreenPresentation(Context outerContext, Display display) {
+        super(outerContext, display);
+    }
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.video_layout);
+        WebView webView = findViewById(R.id.YoutubeTV);
+        String url = "https://youtube.com/tv";
+        CookieSyncManager.createInstance(webView.getContext());
+        CookieManager cookieManager = CookieManager.getInstance();
+        cookieManager.removeSessionCookie();
+        cookieManager.setCookie("youtube.com", "cookies-state=accepted");
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            cookieManager.flush();
+        } else {
+            CookieSyncManager.getInstance().sync();
+        }
+        webView.loadUrl(url);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setUserAgentString("Mozilla/5.0 (Web0S; Linux/SmartTV) AppleWebKit/537.36 (KHTML, like Gecko) QtWebEngine/5.2.1 Chr0me/38.0.2125.122 Safari/537.36 LG Browser/8.00.00(LGE; 60UH6550-UB; 03.00.15; 1; DTV_W16N); webOS.TV-2016; LG NetCast.TV-2013 Compatible (LGE, 60UH6550-UB, wireless)");
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        webView.setWebViewClient(new WebViewClient() {
+            public void onPageFinished(final WebView webView, String url) {
+                Handler handler = new Handler();
+                handler.postDelayed(() -> {
+                    webView.requestFocus();
+                    dispatchKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_DPAD_LEFT));
+                    dispatchKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_DPAD_DOWN));
+                    dispatchKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_DPAD_DOWN));
+                    dispatchKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_DPAD_DOWN));
+                    dispatchKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_DPAD_DOWN));
+                    dispatchKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_DPAD_DOWN));
+                    dispatchKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_DPAD_DOWN));
+                    dispatchKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_DPAD_DOWN));
+                }, 1000);
+                Handler handler1 = new Handler();
+                handler1.postDelayed(() -> {
+                    webView.requestFocus();
+                    dispatchKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_DPAD_RIGHT));
+                    dispatchKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_DPAD_DOWN));
+                    dispatchKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_DPAD_UP));
+                }, 2000);
+            }
+
+        });
+    }}
