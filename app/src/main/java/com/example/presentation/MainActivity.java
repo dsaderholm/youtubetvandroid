cbbaeffe0a1409b0aab6e1f19561e375257cf8b6
D.J. Saderholm <catsdgs@disr.it>
2021-12-17 17:44:25 -0700

    gfgd

--- app/src/main/java/com/example/presentation/MainActivity.java
+++ app/src/main/java/com/example/presentation/MainActivity.java
@@ -5,15 +5,19 @@ import android.hardware.display.DisplayManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Display;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private DisplayManager displayManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initVideo();
+        Toast.makeText(getApplicationContext(), "YouTube for TV launched on connected screen. Please restart the app if the display disconnects.", Toast.LENGTH_LONG).show();
     }
+
     private void initVideo() {
         if (displayManager == null) {
             displayManager = (DisplayManager) getSystemService(Context.DISPLAY_SERVICE);
